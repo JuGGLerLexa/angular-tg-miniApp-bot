8727275e19d08f9aec842196702b5bca3f9fab0f
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2023-12-07 01:18:09 +0200

    initial commt

---
+++ main.js
@@ -0,0 +1,16 @@
+import { Telegraf, Markup } from "telegraf";
+
+const token = "6924398275:AAGHIh86fdgQEll_B6WAiK20NLA5jvz1wA8";
+
+const bot = new Telegraf(token);
+
+bot.command("start", (ctx) => {
+  ctx.reply(
+    "Ласкаво просимо! Нажміть на кнопку нижче, щоб запустити застосунок",
+    Markup.keyboard([
+      Markup.button.webApp("Відправити повідомлення", webAppUrl),
+    ])
+  );
+});
+
+bot.launch();
